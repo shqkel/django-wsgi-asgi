3f16a9523ccdef9c1ccb8c94de64889569cebd44
shqkel <shqkel1863@gmail.com>
2025-08-05 16:22:18 +0900

    Add files via upload

--- _10_03_django_asgi/_10_03_django_asgi/settings.py
+++ _10_03_django_asgi/_10_03_django_asgi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,9 @@ SECRET_KEY = 'django-insecure-(0%2$qs^u2awva+y^0c0ukhl*pzrcmw69%cv^6g6+mojwj3zjv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOST = os.getenv("ALLOWED_HOST", "127.0.0.1")
+ALLOWED_HOSTS = [ALLOWED_HOST, ]
 
 
 # Application definition
